3d198b6ea1b0b1c481d7ec57a5b998cfb8c09418
blondine-sor <blondinekeumani@teccart.online>
2024-10-10 18:02:01 -0400

    News retrieval Success
    Local news retrieval api by city name

---
+++ main.py
@@ -0,0 +1,99 @@
+import os
+# external_api/main.py
+from fastapi import FastAPI
+from datetime import datetime
+
+import worldnewsapi.configuration
+from dotenv import load_dotenv
+import requests
+import uvicorn
+import worldnewsapi
+from worldnewsapi.models.get_geo_coordinates200_response import GetGeoCoordinates200Response
+from worldnewsapi.models.retrieve_newspaper_front_page200_response import RetrieveNewspaperFrontPage200Response
+from worldnewsapi.rest import ApiException
+from pprint import pprint
+load_dotenv()
+
+app = FastAPI()
+
+
+#function to get city  geocde
+def getcity_geocode(city):
+    
+    url = f"https://api.worldnewsapi.com/geo-coordinates?location={city}"
+    api_key = os.environ["API_KEY"]
+
+    headers = {
+        'x-api-key': api_key
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        geocode = str(data['latitude'])+', '+ str(data['longitude']) +', 20'
+        
+
+
+
+        return(geocode)
+    else:
+        return f"Error: {response.status_code}"
+    
+def get_news():
+
+    # configuration = worldnewsapi.configuration(
+    #     host = "https://api.worldnewsapi.com"
+    # ) 
+    #configure date of article
+    current_datetime = datetime.now()
+    formatted_date = current_datetime.strftime("%Y-%m-%d")
+
+    # Add authorization by using APIKey 
+    # load api key from .env file
+    #configuration.api_key['api_key'] = os.environ["API_KEY"]
+    api_key= os.environ["API_KEY"]
+     
+     #Configurate news API
+    newsapi_configuration = worldnewsapi.Configuration(api_key={'apiKey':api_key})
+    
+    try: 
+        #create news instance to get top news of given location
+        newsapi_instance = worldnewsapi.NewsApi(worldnewsapi.ApiClient(newsapi_configuration))
+
+        location = newsapi_instance.get_geo_coordinates('Montreal')
+
+
+        response = newsapi_instance.search_news(
+                text='artificial intelligence',
+                source_countries='ca,us',
+                location_filter= str(location.latitude)+', '+str(location.longitude)+',30',
+                language='en',
+                latest_publish_date=formatted_date,
+                number=1
+                )
+        
+        print(response)
+        return response.model_dump_json()
+
+    except worldnewsapi.ApiException as e:
+        print("Exception when calling NewsApi->search_news: %s\n" % e)
+
+   
+
+    
+
+        
+
+   
+
+@app.get("/news")
+
+async def get_localnew():
+    response = get_news()
+
+    return response
+
+if __name__=='__main__':
+     uvicorn.run(app,'0.0.0.0', port=8033, workers=2)
---
+++ start.sh
@@ -0,0 +1,4 @@
+
+# command to run api
+
+uvicorn main:app --host 0.0.0.0 --port 8033 --workers 2
